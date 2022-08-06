1c35a52d94ae25cd4ed15008371c0bc9ac33af71
Jay Llacuna <626146+jllacuna@users.noreply.github.com>
2022-08-06 15:49:57 -0700

    Added git config files Updated install script to generate git message template and symlink git files Added README with instructions to set up supporting git files and to run install.sh

---
+++ README.md
@@ -0,0 +1,21 @@
+# dotfiles
+
+My dotfiles
+
+## Instructions
+
+1. Create the file `git/.gitconfig.user` and configure your git `user.name` and `user.email`
+
+```
+[user]
+  name = First Last
+  email = ID+username@users.noreply.github.com
+```
+
+2. Create the file `git/gitmessage.url` and enter the URL template to your bug tracker
+
+```
+https://domain.atlassian.net/browse/bug
+```
+
+3. Run `install.sh`
--- install.sh
+++ install.sh
@@ -2,3 +2,10 @@
 
 [ -e $HOME/.zshrc ]    || ln -s $PWD/zsh/.zshrc $HOME/.zshrc
 [ -e $HOME/.zprofile ] || ln -s $PWD/zsh/.zprofile $HOME/.zprofile
+
+giturl=`cat git/gitmessage.url`
+sed "s|<url>|$giturl|" git/gitmessage.template > git/.gitmessage
+
+[ -e $HOME/.gitconfig ]      || ln -s $PWD/git/.gitconfig $HOME/.gitconfig
+[ -e $HOME/.gitconfig.user ] || ln -s $PWD/git/.gitconfig.user $HOME/.gitconfig.user
+[ -e $HOME/.gitmessage ]     || ln -s $PWD/git/.gitmessage $HOME/.gitmessage
