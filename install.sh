7bac8dbebef5fdad49f6dac096e4644acaada906
Jay Llacuna <626146+jllacuna@users.noreply.github.com>
2022-08-06 16:06:23 -0700

    Added tmux config

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env sh
 
+# zsh
 [ -e $HOME/.zshrc ]    || ln -s $PWD/zsh/.zshrc $HOME/.zshrc
 [ -e $HOME/.zprofile ] || ln -s $PWD/zsh/.zprofile $HOME/.zprofile
 
+# git
 giturl=`cat git/gitmessage.url`
 sed "s|<url>|$giturl|" git/gitmessage.template > git/.gitmessage
 
 [ -e $HOME/.gitconfig ]      || ln -s $PWD/git/.gitconfig $HOME/.gitconfig
 [ -e $HOME/.gitconfig.user ] || ln -s $PWD/git/.gitconfig.user $HOME/.gitconfig.user
 [ -e $HOME/.gitmessage ]     || ln -s $PWD/git/.gitmessage $HOME/.gitmessage
+
+# tmux
+[ -e $HOME/.tmux.conf ] || ln -s $PWD/tmux/.tmux.conf $HOME/.tmux.conf
